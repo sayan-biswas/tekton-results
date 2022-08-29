248e2a9b2f154cb3eeeddc0d74a71b7ca0ebf1c1
Sayan Biswas <sabiswas@redhat.com>
2022-08-30 02:24:36 +0530

    stream logs over gRPC
    Proto definitions for logs added
    
    Added terminating newline
    
    Added filter and order_by
    
    Added filter and order_by
    
    stream logs over gRPC

--- cmd/watcher/main.go
+++ cmd/watcher/main.go
@@ -22,6 +22,7 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"k8s.io/client-go/kubernetes"
 	"log"
 	"os"
 	"time"
@@ -70,12 +71,21 @@ func main() {
 	defer conn.Close()
 	results := v1alpha2pb.NewResultsClient(conn)
 
+	config := injection.ParseAndGetRESTConfigOrDie()
+
+	// Create kubernetes clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatalf("failed to create kubernetes client: %v", err)
+	}
+
 	cfg := &reconciler.Config{
+		KubeClient:                   clientset,
 		DisableAnnotationUpdate:      *disableCRDUpdate,
 		CompletedResourceGracePeriod: *completedRunGracePeriod,
 	}
 
-	sharedmain.MainWithContext(injection.WithNamespaceScope(ctx, ""), "watcher",
+	sharedmain.MainWithConfig(injection.WithNamespaceScope(ctx, ""), "watcher", config,
 		func(ctx context.Context, cmw configmap.Watcher) *controller.Impl {
 			return pipelinerun.NewControllerWithConfig(ctx, results, cfg)
 		}, func(ctx context.Context, cmw configmap.Watcher) *controller.Impl {
@@ -90,7 +100,7 @@ func connectToAPIServer(ctx context.Context, apiAddr string, authMode string) (*
 	if err != nil {
 		log.Fatalf("error loading cert pool: %v", err)
 	}
-	cred := credentials.NewClientTLSFromCert(certs, "")
+	cred := credentials.NewClientTLSFromCert(certs, "results.tekton.dev")
 
 	opts := []grpc.DialOption{
 		grpc.WithBlock(),
